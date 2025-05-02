68624ba2889792b4a68ee08e276c12c4ad28649b
gelogelodin <bantiling.angelo@gmail.com>
2025-05-02 10:39:30 +0800

    Feature: Added css animations

--- src/components/ModuleOne/ModuleOne.tsx
+++ src/components/ModuleOne/ModuleOne.tsx
@@ -7,25 +7,35 @@ import './ModuleOne.scss'
 
 function ModuleOne() {
 
+  function logLink(link:string){
+    console.log(link);
+  }
+
   return (
     <>
       <Container className="module-one-container" fluid>
         <Row>
-          <Col className='no-padding-right' xs={12} md="4">
-            <Image className='full-height' src={Food1Image} />
+          <Col className='overflow-hidden no-padding-right column-one' xs={12} md="4">
+            <a href='#' onClick={() => logLink('food 1')}>
+              <Image className='full-height' src={Food1Image} />
+            </a>
           </Col>
-          <Col className='no-padding-left' xs={12} md="4">
+          <Col className='no-padding-left column-two' xs={12} md="4">
             <div className='mb-2'>
-              <Image className='half-height' src={Food2Image} />
+              <a href='#' onClick={() => logLink('food 2')}>
+                <Image className='half-height' src={Food2Image} />
+              </a>
             </div>
             <div>
-              <Image className='half-height' src={Food3Image} />
+              <a href='#' onClick={() => logLink('food 3')}>
+                <Image className='half-height' src={Food3Image} />
+              </a>
             </div>
           </Col>
-          <Col xs={12} md="4">
+          <Col className='column-three' xs={12} md="4">
             <div className='module-title'>What Does Cooking <br /> Mean?</div>
             <div className='module-description'>Is it simply applying heat to a food product? A way of making certain food safe to eat? Or a way to create flavour and make food more appealing? This is just part of what Hervé This, the father of molecular gastronomy, has dedicated his life to finding out. We spoke to him to find out what his experiments have told him. And in the process even discovered the secret to cooking the perfect egg...</div>
-            <div className='small-title'>THE PERFECT EGG</div>
+            <a onClick={() => logLink('The Perfect Egg')} href="#" className='small-title'>THE PERFECT EGG</a>
             <div className='reminder'>Keep water between 67 and 68°C for a flavourful, tender yolk</div>
           </Col>
         </Row>
--- src/components/ModuleTwo/ModuleTwo.tsx
+++ src/components/ModuleTwo/ModuleTwo.tsx
@@ -7,6 +7,10 @@ import './ModuleTwo.scss'
 
 function ModuleTwo() {
 
+  function logLink(link:string){
+    console.log(link);
+  }
+
   return (
     <>
       
@@ -16,17 +20,23 @@ function ModuleTwo() {
             <div className='module-title color-title'>Taste the Colours</div>
           </Col>
           <Col xs md="4">
-            <Image className='color-image' src={FoodRedImage} />
+            <a href='#' onClick={() => logLink('food red')}>
+              <Image className='color-image' src={FoodRedImage} />
+            </a>
             <div className='module-subtitle'>Red</div>
             <div className='module-description text-center'>Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.</div>
           </Col>
           <Col xs md="4">
+            <a href='#' onClick={() => logLink('food green')}>
             <Image className='color-image' src={FoodGreenImage} />
+            </a>
             <div className='module-subtitle'>Green</div>
             <div className='module-description text-center'>Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours</div>
           </Col>
           <Col xs md="4">
+            <a href='#' onClick={() => logLink('food white')}>
             <Image className='color-image' src={FoodWhiteImage} />
+            </a>
             <div className='module-subtitle'>White</div>
             <div className='module-description text-center'>White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat.</div>
           </Col>
